28ed6a9091b553a1a689367c0a1ce16c9f2cfdd2
dolanske <dolanovsky@gmail.com>
2024-01-28 13:47:47 +0200

    Update package info, add build

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/router.ts'),
+      name: 'crumbs',
+      formats: ['es'],
+      fileName: 'crumbs',
+    },
+  },
+  plugins: [dts({ rollupTypes: true })],
+})
